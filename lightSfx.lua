b8fb180d3903047bb94d18c205ed8f21dd4775aa
Tupo <denduhadelaetstrimy@gmail.com>
2024-12-18 17:20:41 +0300

    Create lightSfx.lua

---
+++ lightSfx.lua
@@ -0,0 +1,5 @@
+local sound = game.ReplicatedStorage.Sounds
+
+sound.BulbBreak.SoundId = "rbxassetid://2612674531"
+sound.BulbZap.SoundId = "rbxassetid://4288784832"
+sound.BulbCharge.SoundId = "rbxassetid://166047422"
